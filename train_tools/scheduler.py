f4cb7483ead3dcaf0d9175ecc26a33b79ae2f6c5
raymin0223 <bsmn0223@kaist.ac.kr>
2021-09-07 23:32:21 +0900

    cosine learning rate scheduler
    Signed-off-by: raymin0223 <bsmn0223@kaist.ac.kr>

--- train_tools/scheduler.py
+++ train_tools/scheduler.py
@@ -1,7 +1,7 @@
 import math
 import numpy as np
 
-__all__ = ['multistep_lr_scheduler']
+__all__ = ['multistep_lr_scheduler', 'cosine_lr_scheduler']
 
 
 def multistep_lr_scheduler(args, optimizer, epoch):
@@ -9,6 +9,18 @@ def multistep_lr_scheduler(args, optimizer, epoch):
     steps = np.sum(epoch > np.asarray(args.milestones))
     if steps > 0:
         lr = lr * (args.lr_decay ** steps)
+            
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+        
+    return optimizer
+
+
+def cosine_lr_scheduler(args, optimizer, epoch):
+    lr = args.local_lr
+    eta_min = lr * (args.lr_decay ** 3)
+    lr = eta_min + (lr - eta_min) * (
+            1 + math.cos(math.pi * epoch / args.num_epochs)) / 2
             
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
