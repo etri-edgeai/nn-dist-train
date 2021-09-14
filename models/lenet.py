dcfff9f4c0c7bbd040359107d918b0acbec35820
raymin0223 <bsmn0223@kaist.ac.kr>
2021-09-14 21:13:31 +0900

    delete LeNetContainer
    Signed-off-by: raymin0223 <bsmn0223@kaist.ac.kr>

--- models/lenet.py
+++ models/lenet.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-__all__=['LeNet', 'LeNetContainer']
+__all__=['LeNet']
 
 
 class LeNet(nn.Module):
@@ -17,31 +17,6 @@ class LeNet(nn.Module):
         else: # landmark
             self.fc1 = nn.Linear(53*53*50, 500)
         self.fc2 = nn.Linear(500, num_classes)
-        
-    def forward(self, x):
-        x = self.conv1(x)
-        x = F.max_pool2d(x, 2, 2)
-        x = F.relu(x)
-        
-        x = self.conv2(x)
-        x = F.max_pool2d(x, 2, 2)
-        x = F.relu(x)
-        
-        x = x.view(x.size(0), -1)
-        x = self.fc1(x)
-        x = self.fc2(x)
-        
-        return x
-
-    
-class LeNetContainer(nn.Module):
-    def __init__(self, num_filters, kernel_size, input_dim, hidden_dims, in_channels=1, num_classes=10):
-        super(LeNetContainer, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, num_filters[0], kernel_size, 1)
-        self.conv2 = nn.Conv2d(num_filters[0], num_filters[1], kernel_size, 1)
-
-        self.fc1 = nn.Linear(input_dim, hidden_dims[0])
-        self.fc2 = nn.Linear(hidden_dims[0], num_classes)
         
     def forward(self, x):
         x = self.conv1(x)
