2616953886f8af9c4ebe43ad140241799184bb1b
jaeyeon <dkswodus49@kaist.ac.kr>
2021-11-05 11:23:11 +0900

    added some descriptions
    Signed-off-by: jaeyeon <dkswodus49@kaist.ac.kr>

--- FedLSD/traintools/measure_utils/cka.py
+++ FedLSD/traintools/measure_utils/cka.py
@@ -4,10 +4,31 @@ __all__ = ["gram_linear", "gram_rbf", "center_gram", "cka", "feature_space_linea
 
 
 def gram_linear(x):
+    """
+    Compute Gram (kernel) matrix for a linear kernel.
+
+    Args:
+    x: A num_examples x num_features matrix of features.
+
+    Returns:
+    A num_examples x num_examples Gram matrix of examples.
+    """
     return torch.matmul(x, x.T)
 
 
 def gram_rbf(x, threshold=1.0):
+    """
+    Compute Gram (kernel) matrix for an RBF kernel.
+
+    Args:
+    x: A num_examples x num_features matrix of features.
+    threshold: Fraction of median Euclidean distance to use as RBF kernel
+      bandwidth. (This is the heuristic we use in the paper. There are other
+      possible ways to set the bandwidth; we didn't try them.)
+
+    Returns:
+    A num_examples x num_examples Gram matrix of examples.
+    """
     dot_products = torch.matmul(x, x.T)
     sq_norms = torch.diag(dot_products)
     sq_distances = -2 * dot_products + sq_norms[:, None] + sq_norms[None, :]
@@ -16,11 +37,28 @@ def gram_rbf(x, threshold=1.0):
 
 
 def center_gram(gram, unbiased=False):
+    """Center a symmetric Gram matrix.
+
+    This is equvialent to centering the (possibly infinite-dimensional) features
+    induced by the kernel before computing the Gram matrix.
+
+    Args:
+    gram: A num_examples x num_examples symmetric matrix.
+    unbiased: Whether to adjust the Gram matrix in order to compute an unbiased
+      estimate of HSIC. Note that this estimator may be negative.
+
+    Returns:
+    A symmetric matrix with centered columns and rows.
+    """
     if not torch.allclose(gram, gram.T):
         raise ValueError("Input must be a symmetric matrix.")
     gram = gram.detach()
 
     if unbiased:
+        # This formulation of the U-statistic, from Szekely, G. J., & Rizzo, M.
+        # L. (2014). Partial distance correlation with methods for dissimilarities.
+        # The Annals of Statistics, 42(6), 2382-2412, seems to be more numerically
+        # stable than the alternative from Song et al. (2007).
         n = gram.shape[0]
         torch.fill_diagonal(gram, 0)
         means = torch.sum(gram, 0, dtype=torch.float64) / (n - 2)
@@ -38,6 +76,17 @@ def center_gram(gram, unbiased=False):
 
 
 def cka(gram_x, gram_y, debiased=False):
+    """
+    Compute CKA.
+
+    Args:
+    gram_x: A num_examples x num_examples Gram matrix.
+    gram_y: A num_examples x num_examples Gram matrix.
+    debiased: Use unbiased estimator of HSIC. CKA may still be biased.
+
+    Returns:
+    The value of CKA between X and Y.
+    """
     gram_x = center_gram(gram_x, unbiased=debiased)
     gram_y = center_gram(gram_y, unbiased=debiased)
 
@@ -54,6 +103,11 @@ def cka(gram_x, gram_y, debiased=False):
 def _debiased_dot_product_similarity_helper(
     xty, sum_squared_rows_x, sum_squared_rows_y, squared_norm_x, squared_norm_y, n
 ):
+    """
+    Helper for computing debiased dot product similarity (i.e. linear HSIC).
+    """
+    # This formula can be derived by manipulating the unbiased estimator from
+    # Song et al. (2007).
     return (
         xty
         - n / (n - 2.0) * sum_squared_rows_x.dot(sum_squared_rows_y)
@@ -62,6 +116,21 @@ def _debiased_dot_product_similarity_helper(
 
 
 def feature_space_linear_cka(features_x, features_y, debiased=False):
+    """
+    Compute CKA with a linear kernel, in feature space.
+
+    This is typically faster than computing the Gram matrix when there are fewer
+    features than examples.
+
+    Args:
+    features_x: A num_examples x num_features matrix of features.
+    features_y: A num_examples x num_features matrix of features.
+    debiased: Use unbiased estimator of dot product similarity. CKA may still be
+      biased. Note that this estimator may be negative.
+
+    Returns:
+    The value of CKA between X and Y.
+    """
     features_x = features_x - torch.mean(features_x, 0, keepdims=True)
     features_y = features_y - torch.mean(features_y, 0, keepdims=True)
 
