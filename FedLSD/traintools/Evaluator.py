c6d233a070d1c873cd0de76ce73dd6d85a8c7f21
jaeyeon <dkswodus49@kaist.ac.kr>
2021-11-05 11:37:45 +0900

    added utils code for evaluator
    Signed-off-by: jaeyeon <dkswodus49@kaist.ac.kr>

---
+++ FedLSD/traintools/Evaluator.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from .measure_utils import *
+from .measures import *
+from .utils import *
+
+import wandb
+
+__all__ = ["Evaluator"]
+
+
+class Evaluator:
+    """Evaluate statistics for analysis"""
+
+    def __init__(self):
+
+
+    def evaluation(self):
+        return
+
+    def global_evaluation(self):
+        return
