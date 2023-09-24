73ba820553d93bfb73f1b09ce3018ffb33e46375
WoojinChung <58346392+gartland98@users.noreply.github.com>
2023-09-24 17:50:50 +0900

    Add files via upload

---
+++ nlp/constants.py
@@ -0,0 +1,409 @@
+
+
+LANG_MAP_EUROPARL = {
+    "en": 0,
+    "cs": 1,
+    "lt": 2,
+    "es": 3,
+    "pl": 4,
+    "fi": 5,
+    "pt": 6,
+    "de": 7
+}
+
+
+LANG_MAP_UN_CORPUS = {
+    "en": 0,
+    "es": 1,
+    "zh": 2,
+    "ru": 3,
+    "ar": 4,
+    "fr": 5,
+}
+
+LANG_MAP_PAWSX = {
+    "de": 0,
+    "en": 1,
+    "es": 2,
+    "fr": 3,
+    "ja": 4,
+    "ko": 5,
+    "zh": 6,
+}
+
+
+LANG_MAP_UN_MT_CORPUS = {
+    "en-fr": 0,
+    "ar-es": 1,
+    "ru-zh": 2,
+}
+
+LANG_MAP_MTNT_MT_CORPUS = {
+    "en-fr": 0,
+    "en-ja": 1,
+}
+
+
+LANG_MAP_NC = {
+    "en": 0,
+    "es": 1,
+    "fr": 2,
+    "de": 3,
+    
+}
+
+LANG_MAP_RTE = {
+    "1": 0,
+    "2": 1,
+    "3": 2,
+    
+}
+
+LANG_MAP_COLA = {
+    "1": 0,
+    "2": 1,
+    "3": 2,
+    
+}
+
+LANG_MAP_SST2 = {
+    "1": 0,
+    "2": 1,
+    "3": 2,
+    
+}
+LANG_MAP_QNLI = {
+    "1": 0,
+    "2": 1,
+    "3": 2,
+    
+}
+LANG_MAP_WNLI = {
+    "1": 0,
+    "2": 1,
+    "3": 2,
+    
+}
+LANG_MAP_BOOLQ = {
+    "1": 0,
+    "2": 1,
+    "3": 2,
+    
+}
+
+LANG_MAP_XNLI = {
+    "en": 0,
+    "fr": 1,
+    "es": 2,
+    "de":3,
+    "el":4,
+    "bg":5,
+    "ru":6,
+    "tr":7,
+    "ar":8,
+    "vi":9,
+    "th":10,
+    "zh":11,
+    "hi":12,
+    "sw":13,
+    "ur":14,    
+}
+'''
+LANG_MAP_XNLI = {
+    "en1": 0,
+    "en2": 1,
+    "fr1": 2,
+    "fr2": 3,
+    "es1": 4,
+    "es2": 5,
+    "de1": 6,
+    "de2": 7,
+    "el1": 8,
+    "el2": 9,
+    "bg1":10,
+    "bg2":11,
+    "ru1":12,
+    "ru2":13,    
+    "tr1":14,
+    "tr2":15,   
+    "ar1":16,
+    "ar2":17,
+    "vi1":18,
+    "vi2":19,
+    "th1":20,
+    "th2":21,
+    "zh1":22,
+    "zh2":23,
+    "hi1":24,
+    "hi2":25,
+    "sw1":26,
+    "sw2":27,
+    "ur1":28,
+    "ur2":29,
+}
+
+LANG_MAP_XNLI = {
+    "en1": 0,
+    "en2": 1,
+    "en3": 2,
+    "fr1": 3,
+    "fr2": 4,
+    "fr3": 5,
+    "es1": 6,
+    "es2": 7,
+    "es3": 8,
+    "de1": 9,
+    "de2":10,
+    "de3":11,
+    "el1":12,
+    "el2":13,
+    "el3":14,
+    "bg1":15,
+    "bg2":16,
+    "bg3":17,
+    "ru1":18,
+    "ru2":19,
+    "ru3":20,
+    "tr1":21,
+    "tr2":22,
+    "tr3":23,
+    "ar1":24,
+    "ar2":25,
+    "ar3":26,
+    "vi1":27,
+    "vi2":28,
+    "vi3":29,
+    "th1":30,
+    "th2":31,
+    "th3":32,
+    "zh1":33,
+    "zh2":34,
+    "zh3":35,
+    "hi1":36,
+    "hi2":37,
+    "hi3":38,
+    "sw1":39,
+    "sw2":40,
+    "sw3":41,
+    "ur1":42,
+    "ur2":43,
+    "ur3":44,
+}
+
+
+LANG_MAP_XNLI = {
+    "en1": 0,
+    "en2": 1,
+    "en3": 2,
+    "en4": 3,
+    "fr1": 4,
+    "fr2": 5,
+    "fr3": 6,
+    "fr4": 7,
+    "es1": 8,
+    "es2": 9,
+    "es3":10,
+    "es4":11,
+    "de1":12,
+    "de2":13,
+    "de3":14,
+    "de4":15,
+    "el1":16,
+    "el2":17,
+    "el3":18,
+    "el4":19,
+    "bg1":20,
+    "bg2":21,
+    "bg3":22,
+    "bg4":23,    
+    "ru1":24,
+    "ru2":25,
+    "ru3":26,
+    "ru4":27,    
+    "tr1":28,
+    "tr2":29,
+    "tr3":30,
+    "tr4":31,    
+    "ar1":32,
+    "ar2":33,
+    "ar3":34,
+    "ar4":35,
+    "vi1":36,
+    "vi2":37,
+    "vi3":38,
+    "vi4":39,
+    "th1":40,
+    "th2":41,
+    "th3":42,
+    "th4":43,
+    "zh1":44,
+    "zh2":45,
+    "zh3":46,
+    "zh4":47,
+    "hi1":48,
+    "hi2":49,
+    "hi3":50,
+    "hi4":51,
+    "sw1":52,
+    "sw2":53,
+    "sw3":54,
+    "sw4":55,
+    "ur1":56,
+    "ur2":57,
+    "ur3":58,
+    "ur4":59,
+}
+'''
+LANG_MAP_MNLI = {
+    "fic": 0,
+    "gov": 1,
+    "slate": 2,
+    "tel":3,
+    "travel":4,   
+}
+'''
+LANG_MAP_MNLI = {
+    "fic1": 0,
+    "fic2": 1,
+    "gov1": 2,
+    "gov2": 3,
+    "slate1": 4,
+    "slate2": 5,
+    "tel1":6,
+    "tel2": 7,
+    "travel1":8,
+    "travel2": 9,
+}
+
+LANG_MAP_MNLI = {
+    "fic1": 0,
+    "fic2": 1,
+    "fic3": 2,
+    "fic4": 3,
+    "gov1": 4,
+    "gov2": 5,
+    "gov3": 6,
+    "gov4": 7,
+    "slate1": 8,
+    "slate2": 9,
+    "slate3": 10,
+    "slate4": 11,
+    "tel1":12,
+    "tel2": 13,
+    "tel3":14,
+    "tel4": 15,
+    "travel1":16,
+    "travel2": 17,
+    "travel3":18,
+    "travel4": 19,
+}
+
+LANG_MAP_MNLI = {
+    "fic1": 0,
+    "fic2": 1,
+    "fic3": 2,
+    "fic4": 3,
+    "fic5": 4,
+    "fic6": 5,
+    "fic7": 6,
+    "fic8": 7,
+    "gov1": 8,
+    "gov2": 9,
+    "gov3": 10,
+    "gov4": 11,
+    "gov5": 12,
+    "gov6": 13,
+    "gov7": 14,
+    "gov8": 15,
+    "slate1": 16,
+    "slate2": 17,
+    "slate3": 18,
+    "slate4": 19,
+    "slate5": 20,
+    "slate6": 21,
+    "slate7": 22,
+    "slate8": 23,
+    "tel1":24,
+    "tel2":25,
+    "tel3":26,
+    "tel4": 27,
+    "tel5":28,
+    "tel6": 29,
+    "tel7":30,
+    "tel8": 31,
+    "travel1":32,
+    "travel2": 33,
+    "travel3":34,
+    "travel4": 35,
+    "travel5":36,
+    "travel6": 37,
+    "travel7":38,
+    "travel8": 39,
+}
+'''
+
+MBART_MAP = {
+    "en": "en_XX",
+    "cs": "cs_CZ",
+    "lt": "lt_LT",
+    "es": "es_XX",
+    "pl": "pl_PL",
+    "fi": "fi_FI",
+    "pt": "pt_XX",
+    "de": "de_DE",
+}
+
+
+POOL_SIZE = {
+    "brown": 1,
+    "wmt": 8,
+    "un_corpus": 6,
+    "un_mt_corpus": 3,
+    "mtnt": 2,
+    "pawsx": 7,
+    "nc": 4,
+    "xnli": 15,
+    "mnli":5,
+    "rte":3,
+    "cola":3,
+    "sst2":3,
+    "qnli":3,
+    "wnli":3,
+    "boolq":3,
+    
+}
+
+DATA_TO_FILE_PATHS = {
+    "brown": "data/brown",
+    "wmt": "data/wmt",
+    "un_corpus": 'data/un_corpus',
+    "un_mt_corpus": 'data/un_mt_corpus',
+    "mtnt": "data/mtnt_mt_corpus",
+    "pawsx": "data/pawsx",
+    "nc": "data/nc",
+    "xnli": "data/xnli",
+    "mnli": "data/mnli",
+    "rte": "data/rte",
+    "cola": "data/cola",
+    "sst2": "data/sst2",
+    "qnli": "data/qnli",
+    "wnli": "data/wnli",
+    "boolq": "data/boolq",
+}
+
+MAP_LANG_MAP = {
+    "data/wmt": LANG_MAP_EUROPARL,
+    "data/un_corpus": LANG_MAP_UN_CORPUS,
+    "data/un_mt_corpus": LANG_MAP_UN_MT_CORPUS,
+    "data/mtnt_mt_corpus": LANG_MAP_MTNT_MT_CORPUS,
+    "data/pawsx": LANG_MAP_PAWSX,
+    "data/nc": LANG_MAP_NC,
+    "data/xnli": LANG_MAP_XNLI,
+    "data/mnli": LANG_MAP_MNLI,
+    "data/rte": LANG_MAP_RTE,
+    "data/cola": LANG_MAP_COLA,
+    "data/sst2": LANG_MAP_SST2,
+    "data/qnli": LANG_MAP_QNLI,
+    "data/wnli": LANG_MAP_WNLI,
+    "data/boolq": LANG_MAP_BOOLQ,
+}
