4f602f13402947ac760fcb973cfda538648ccfb4
seongyoon <curisam@kaist.ac.kr>
2024-06-16 18:01:49 +0900

    generate fedntd.sh

--- image_classification/gfl/fedntd.sh
+++ image_classification/gfl/fedntd.sh
@@ -1,3 +1,26 @@
+#Shard
+
+
+#CIFAR10
+
+
+python main.py --config_path ./config/cifar10/fedntd.json --partition_method sharding --partition_s 2 --device cuda:5 --seed 100
+python main.py --config_path ./config/cifar10/fedntd.json --partition_method sharding --partition_s 5 --device cuda:5 --seed 100
+python main.py --config_path ./config/cifar10/fedntd.json --partition_method sharding --partition_s 10 --device cuda:5 --seed 100
+
+
+
+
+#CIFAR100
+
+
+python main.py --config_path ./config/cifar100/fedntd.json --partition_s 10 --device cuda:5 --seed 100
+python main.py --config_path ./config/cifar100/fedntd.json --partition_s 20 --device cuda:5 --seed 100
+python main.py --config_path ./config/cifar100/fedntd.json --partition_s 50 --device cuda:5 --seed 100
+python main.py --config_path ./config/cifar100/fedntd.json --partition_s 100 --device cuda:5 --seed 100
+
+
+#LDA
 #CIFAR10
 
 python main.py --config_path ./config/cifar10/fedntd.json --partition_method lda --partition_alpha 0.1  --device cuda:5 --seed 100
@@ -12,5 +35,6 @@ python main.py --config_path ./config/cifar100/fedntd.json --partition_method ld
 python main.py --config_path ./config/cifar100/fedntd.json --partition_method lda --partition_alpha 0.1  --device cuda:5 --seed 100
 python main.py --config_path ./config/cifar100/fedntd.json --partition_method lda --partition_alpha 0.2  --device cuda:5 --seed 100
 python main.py --config_path ./config/cifar100/fedntd.json --partition_method lda --partition_alpha 0.3  --device cuda:5 --seed 100
+
 
 
