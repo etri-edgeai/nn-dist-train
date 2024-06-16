5368c5bd4c49c0c24f5ad9361cb56db57e56cf45
seongyoon <curisam@kaist.ac.kr>
2024-06-16 23:47:01 +0900

    generate fedavg_cifar100_shard.sh

--- image_classification/gfl/fedavg.sh
+++ image_classification/gfl/fedavg.sh
@@ -1,21 +1,6 @@
-#CIFAR100
-
-python main.py --config_path ./config/cifar100/fedavg.json --partition_s 10 --device cuda:5 --seed 100
-python main.py --config_path ./config/cifar100/fedavg.json --partition_s 20 --device cuda:5 --seed 100
-python main.py --config_path ./config/cifar100/fedavg.json --partition_s 50 --device cuda:5 --seed 100
-python main.py --config_path ./config/cifar100/fedavg.json --partition_s 100 --device cuda:5 --seed 100
 
 
 #LDA
-#CIFAR10
-
-python main.py --config_path ./config/cifar10/fedavg.json --partition_method lda --partition_alpha 0.1  --device cuda:6 --seed 100
-python main.py --config_path ./config/cifar10/fedavg.json --partition_method lda --partition_alpha 0.2  --device cuda:6 --seed 100
-python main.py --config_path ./config/cifar10/fedavg.json --partition_method lda --partition_alpha 0.3  --device cuda:6 --seed 100
-
-
-
-
 #CIFAR100
 
 python main.py --config_path ./config/cifar100/fedavg.json --partition_method lda --partition_alpha 0.05  --device cuda:6 --seed 100
