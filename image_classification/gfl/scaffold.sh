971b7ba50dd60a5cf90ea9fa4aab708625d71e14
seongyoon <curisam@kaist.ac.kr>
2024-06-16 18:37:01 +0900

    generate scaffold.sh

--- image_classification/gfl/scaffold.sh
+++ image_classification/gfl/scaffold.sh
@@ -1,3 +1,28 @@
+#Shard
+
+
+#CIFAR10
+
+
+
+python main.py --config_path ./config/cifar10/scaffold.json --partition_method sharding --partition_s 2 --device cuda:6 --seed 100
+python main.py --config_path ./config/cifar10/scaffold.json --partition_method sharding --partition_s 5 --device cuda:6 --seed 100
+python main.py --config_path ./config/cifar10/scaffold.json --partition_method sharding --partition_s 10 --device cuda:6 --seed 100
+
+
+
+
+#CIFAR100
+
+python main.py --config_path ./config/cifar100/scaffold.json --partition_s 10 --device cuda:6 --seed 100
+python main.py --config_path ./config/cifar100/scaffold.json --partition_s 20 --device cuda:6 --seed 100
+python main.py --config_path ./config/cifar100/scaffold.json --partition_s 50 --device cuda:6 --seed 100
+python main.py --config_path ./config/cifar100/scaffold.json --partition_s 100 --device cuda:6 --seed 100
+
+
+#LDA
+
+
 #CIFAR10
 
 python main.py --config_path ./config/cifar10/scaffold.json --partition_method lda --partition_alpha 0.1  --device cuda:6 --seed 100
