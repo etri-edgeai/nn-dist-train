117f18d59fd2c2e468debd951bd7edc70e104dd5
seongyoon <curisam@kaist.ac.kr>
2024-06-16 23:34:06 +0900

    generate fedavg_cifar10_shard.sh

--- image_classification/gfl/fedavg.sh
+++ image_classification/gfl/fedavg.sh
@@ -1,15 +1,3 @@
-#Shard
-
-
-#CIFAR10
-
-python main.py --config_path ./config/cifar10/fedavg.json --partition_method sharding --partition_s 2 --device cuda:5 --seed 100
-python main.py --config_path ./config/cifar10/fedavg.json --partition_method sharding --partition_s 5 --device cuda:5 --seed 100
-python main.py --config_path ./config/cifar10/fedavg.json --partition_method sharding --partition_s 10 --device cuda:5 --seed 100
-
-
-
-
 #CIFAR100
 
 python main.py --config_path ./config/cifar100/fedavg.json --partition_s 10 --device cuda:5 --seed 100
