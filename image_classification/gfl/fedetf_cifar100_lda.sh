117617d5e69e2ec17470b782c350170b4bb16a18
seongyoon <curisam@kaist.ac.kr>
2024-06-23 04:08:22 +0900

    generate fedetf_cifar100.sh

--- image_classification/gfl/fedetf_cifar100_lda.sh
+++ image_classification/gfl/fedetf_cifar100_lda.sh
@@ -1,18 +1,7 @@
-
-
-
-#CIFAR100
-python main.py --config_path ./config/cifar100/fedetf.json --partition_s 10 --device cuda:3 --seed 100
-python main.py --config_path ./config/cifar100/fedetf.json --partition_s 20 --device cuda:3 --seed 100
-python main.py --config_path ./config/cifar100/fedetf.json --partition_s 50 --device cuda:3 --seed 100
-python main.py --config_path ./config/cifar100/fedetf.json --partition_s 100 --device cuda:3 --seed 100
-
-
 #CIFAR100
 
 python main.py --config_path ./config/cifar100/fedetf.json --partition_method lda --partition_alpha 0.05  --device cuda:3 --seed 100
 python main.py --config_path ./config/cifar100/fedetf.json --partition_method lda --partition_alpha 0.1  --device cuda:3 --seed 100
 python main.py --config_path ./config/cifar100/fedetf.json --partition_method lda --partition_alpha 0.2  --device cuda:3 --seed 100
 python main.py --config_path ./config/cifar100/fedetf.json --partition_method lda --partition_alpha 0.3  --device cuda:3 --seed 100
-
 
