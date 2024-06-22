b65d862ef9e7c48339ba4d401de60082da905665
seongyoon <curisam@kaist.ac.kr>
2024-06-23 00:55:43 +0900

    generate fedbabu_cifar10.sh

---
+++ image_classification/gfl/fedbabu_cifar10_shard.sh
@@ -0,0 +1,8 @@
+#Shard
+
+
+#CIFAR10
+
+python main.py --config_path ./config/cifar10/fedbabu.json --partition_method sharding --partition_s 2 --device cuda:0 --seed 100
+python main.py --config_path ./config/cifar10/fedbabu.json --partition_method sharding --partition_s 5 --device cuda:0 --seed 100
+python main.py --config_path ./config/cifar10/fedbabu.json --partition_method sharding --partition_s 10 --device cuda:0 --seed 100
