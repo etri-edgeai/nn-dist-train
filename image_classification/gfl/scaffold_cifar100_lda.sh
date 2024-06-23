c420ff527f7690820d655c863d28e8ab9048d0af
seongyoon <curisam@kaist.ac.kr>
2024-06-23 15:40:45 +0900

    generate scaffold_cifar100.sh

--- image_classification/gfl/scaffold_cifar100_lda.sh
+++ image_classification/gfl/scaffold_cifar100_lda.sh
@@ -1,10 +1,3 @@
-#CIFAR100
-
-python main.py --config_path ./config/cifar100/scaffold.json --partition_s 10 --device cuda:6 --seed 100
-python main.py --config_path ./config/cifar100/scaffold.json --partition_s 20 --device cuda:6 --seed 100
-python main.py --config_path ./config/cifar100/scaffold.json --partition_s 50 --device cuda:6 --seed 100
-python main.py --config_path ./config/cifar100/scaffold.json --partition_s 100 --device cuda:6 --seed 100
-
 
 #CIFAR100
 
